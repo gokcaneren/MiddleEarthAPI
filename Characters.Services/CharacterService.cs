75cad23532ad4caa36defe795b05c9d5c55cd2df
gokcaneren <gokcann01@gmail.com>
2022-04-29 19:14:30 +0300

    Design Added!

---
+++ Characters.Services/CharacterService.cs
@@ -0,0 +1,33 @@
+﻿using Characters.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters.Services
+{
+    public class CharacterService
+    {
+        private List<Character> characters;
+
+        public CharacterService()
+        {
+            characters = new List<Character>
+            {
+                new Character{Id=1, Name="Aragorn", Age=75, RaceId=2, isAlive=true, ImageUrl="https://static.wikia.nocookie.net/ortadunya/images/3/34/Aragorn2.jpg/revision/latest?cb=20210609105006"},
+                new Character{Id=2, Name="Gandalf", Age=150, RaceId=5, isAlive=true, ImageUrl="https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754"},
+                new Character{Id=3, Name="Boromir", Age=43, RaceId=2, isAlive=false, ImageUrl="https://static.wikia.nocookie.net/lotr/images/b/b4/Seanbean_boromir.jpg/revision/latest?cb=20110327195115"},
+                new Character{Id=4, Name="Legolas", Age=66, RaceId=1, isAlive=true, ImageUrl="https://static.wikia.nocookie.net/middle-earth-film-saga/images/7/77/Legolas.png/revision/latest/top-crop/width/360/height/450?cb=20160207050831"},
+                new Character{Id=5, Name="Witch King", Age=135, RaceId=1, isAlive=false, ImageUrl="https://static.wikia.nocookie.net/lotr/images/5/59/Witch-king.jpg/revision/latest?cb=20220209185252"},
+                new Character{Id=6, Name="Gimli", Age=63, RaceId=3, isAlive=true, ImageUrl="https://static.wikia.nocookie.net/ortadunya/images/4/43/Gimli.jpg/revision/latest/top-crop/width/360/height/450?cb=20190424060619"}
+            };
+        }
+
+        public List<Character> GetAll()
+        {
+            return characters;
+        }
+
+    }
+}
---
+++ MiddleEarthApi/Controllers/CharactersController.cs
@@ -0,0 +1,21 @@
+﻿using Characters.Entities;
+using Characters.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiddleEarthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharactersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var characters = new CharacterService().GetAll();
+            return Ok(characters);
+        }
+
+
+    }
+}
